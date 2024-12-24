88a3f168cac799a370ecc2f905767c448857960c
Collin Diekvoss <Collin@Diekvoss.com>
2024-12-23 20:02:33 -0600

    0.6.1

--- flake.nix
+++ flake.nix
@@ -70,31 +70,71 @@
           };
 
           formatter = pkgs.nixfmt-rfc-style;
-          packages.default =
-            let
-              cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-              rev = self'.shortRev or self'.dirtyShortRev or "dirty";
-            in
-            pkgs.rustPlatform.buildRustPackage {
-              pname = cargoToml.package.name;
-              version = "${cargoToml.package.version}-${rev}";
-              src = ./.;
-              strictDeps = true;
-              buildInputs = rustBuildInputs;
-              nativeBuildInputs = with pkgs; [
-                dioxus-cli
-                rustToolchain
-              ];
-              buildPhase = ''
-                dx build --release --platform web
-              '';
-              installPhase = ''
-                mkdir -p $out
-                cp -r target/dx/$pname/release/web $out/bin
-              '';
-              cargoLock.lockFile = ./Cargo.lock;
-              meta.mainProgram = "server";
+          packages = rec {
+            wasm-bindgen-cli = pkgs.wasm-bindgen-cli.override {
+              version = "0.2.99";
+              hash = "sha256-1AN2E9t/lZhbXdVznhTcniy+7ZzlaEp/gwLEAucs6EA=";
+              cargoHash = "sha256-DbwAh8RJtW38LJp+J9Ht8fAROK9OabaJ85D9C/Vkve4=";
             };
+            dioxus-cli = pkgs.dioxus-cli.overrideAttrs (drv: rec {
+              version = "0.6.1";
+              src = pkgs.fetchCrate {
+                inherit version;
+                pname = drv.pname;
+                hash = "sha256-mQnSduf8SHYyUs6gHfI+JAvpRxYQA1DiMlvNofImElU=";
+              };
+              cargoDeps = drv.cargoDeps.overrideAttrs (
+                lib.const {
+                  name = "${drv.cargoDeps.name}-vendor";
+                  inherit src;
+                  outputHash = "sha256-QiGnBoZV4GZb5MQ3K/PJxCfw0p/7qDmoE607hlGKOns=";
+                }
+              );
+              postFixup =
+                if pkgs.stdenv.isDarwin then
+                  ''
+                    mkdir -p "$out/home/Library/Application Support/dioxus/wasm-bindgen"
+                    ln -s ${lib.getExe wasm-bindgen-cli} "$out/home/Library/Application Support/dioxus/wasm-bindgen/wasm-bindgen-${wasm-bindgen-cli.version}"
+                    wrapProgram $out/bin/dx \
+                      --set HOME $out/home
+                  ''
+                else
+                  ''
+                    mkdir -p $out/share/dioxus/wasm-bindgen
+                    ln -s ${lib.getExe wasm-bindgen-cli} $out/share/dioxus/wasm-bindgen/wasm-bindgen-${wasm-bindgen-cli.version}
+                    wrapProgram $out/bin/dx \
+                      --set XDG_DATA_HOME $out/share
+                  '';
+              checkFlags = drv.checkFlags ++ [ "--skip=wasm_bindgen::test" ];
+              nativeBuildInputs = drv.nativeBuildInputs ++ [ pkgs.makeBinaryWrapper ];
+            });
+            default =
+              let
+                cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+                rev = self'.shortRev or self'.dirtyShortRev or "dirty";
+              in
+              pkgs.rustPlatform.buildRustPackage {
+                pname = cargoToml.package.name;
+                version = "${cargoToml.package.version}-${rev}";
+                src = ./.;
+                strictDeps = true;
+                buildInputs = rustBuildInputs;
+                nativeBuildInputs = [
+                  dioxus-cli
+                  rustToolchain
+                  pkgs.rustPlatform.bindgenHook
+                ] ++ rustBuildInputs;
+                buildPhase = ''
+                  dx build --release --platform web
+                '';
+                installPhase = ''
+                  mkdir -p $out
+                  cp -r target/dx/$pname/release/web $out/bin
+                '';
+                cargoLock.lockFile = ./Cargo.lock;
+                meta.mainProgram = "server";
+              };
+          };
 
           devShells.default = pkgs.mkShell {
             name = "dioxus-dev";
